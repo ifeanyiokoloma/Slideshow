de59d5e306f686c80a3b3478433fefbd2ac4cbd5
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2022-12-18 12:23:49 +0100

    controlled auto slide

--- slider.js
+++ slider.js
@@ -3,12 +3,12 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -22,16 +22,38 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
+    slides[i].style.transform = "translate(-100%)";
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  slides[slideIndex - 1].style.transform = "translate(0)";
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
 
 // Automatic slideshow
-setInterval(function() {
-  plusSlides(1);
-}, 3000); // 3 seconds
+const autoSlide = () => {
+  let slideTimer;
+  const slideContainer = document.getElementById("slideshow-container");
+
+  slideTimer = setInterval(function () {
+    console.log("works");
+    plusSlides(1);
+  }, 6000); // 6 seconds
+
+  slideContainer.addEventListener("mouseenter", async () => {
+    // console.log("clears");
+    clearInterval(slideTimer);
+  });
+
+  slideContainer.addEventListener("mouseleave", async () => {
+    slideTimer = setInterval(function () {
+      // console.log("works");
+      plusSlides(1);
+    }, 6000); // 6 seconds
+  });
+};
+
+autoSlide();
